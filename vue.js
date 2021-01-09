8b519d16a50bab67d35e98e380795349a8fc6d27
Uku Pattak <uku.pattak@remato.ee>
2021-01-09 22:41:30 +0200

    Add correct prettier conf

--- vue.js
+++ vue.js
@@ -1,12 +1,12 @@
 module.exports = {
-  parser: "vue-eslint-parser",
-  parserOptions: {
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
-  },
-  extends: ["plugin:vue/base"],
-  rules: {
-    "vue/no-unused-components": "error",
-    "vue/no-unregistered-components": "error",
-  },
-};
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		parser: '@typescript-eslint/parser',
+		sourceType: 'module',
+	},
+	extends: ['plugin:vue/base'],
+	rules: {
+		'vue/no-unused-components': 'error',
+		'vue/no-unregistered-components': 'error',
+	},
+}
