733eccd0c38b6eaa67e7fb7d62913cf432e62ca9
Uku Pattak <uku.pattak@remato.ee>
2022-10-31 10:18:27 +0200

    Rename rule

--- vue.js
+++ vue.js
@@ -2,7 +2,7 @@ module.exports = {
 	extends: ['plugin:vue/base', './javascript'],
 	rules: {
 		'vue/no-unused-components': 'error',
-		'vue/no-unregistered-components': 'error',
+		'vue/no-undef-components': 'error',
 		'vue/this-in-template': 'error',
 		'vue/attributes-order': [
 			'error',
