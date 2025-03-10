bcccf0fdb6cdeaa72e10637f2783a50e4309af26
samueleiche <samuel@samueleiche.com>
2025-03-10 16:31:57 +0200

    add index entry

---
+++ index.js
@@ -0,0 +1,11 @@
+import vueTsConfig from './vue-typescript'
+import tsConfig from './shared/typescript-eslint.js'
+
+const configs = {
+	typescript: tsConfig,
+	'vue-typescript': vueTsConfig,
+}
+
+export default {
+	configs,
+}
